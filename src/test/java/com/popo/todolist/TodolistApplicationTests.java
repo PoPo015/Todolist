55e3db66243a7564433140a7ce911c3aa770435e
popo <kst005109@naver.com>
2024-06-18 00:03:50 +0900

    feat : 프로젝트 생성

---
+++ src/test/java/com/popo/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
