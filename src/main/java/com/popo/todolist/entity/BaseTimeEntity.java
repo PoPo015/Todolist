1cb6257c433b58f2a53eb4274777f5284c04584f
popo <kst005109@naver.com>
2024-06-19 00:22:27 +0900

    feat : BaseTimeEntity 추가
    - 등록시간은 대부분의 테이블에 존재하기 때문에
    공통 BaseTimeEntity 를 상속받아 각 엔티티들의 중복코드 제거.

---
+++ src/main/java/com/popo/todolist/entity/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.popo.todolist.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "create_dt", updatable = false, nullable = false)
+    private LocalDateTime createDt;
+
+}
