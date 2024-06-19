1a70699cde75378be136dda01e7956744de38c61
popo <kst005109@naver.com>
2024-06-20 01:08:44 +0900

    bugfix : 패스워드 검증 로직 수정

--- src/main/java/com/popo/todolist/service/PasswordService.java
+++ src/main/java/com/popo/todolist/service/PasswordService.java
@@ -1,5 +1,7 @@
 package com.popo.todolist.service;
 
+import com.popo.todolist.common.RootException;
+import com.popo.todolist.common.constants.ResultCodeType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,7 +16,10 @@ public class PasswordService {
         return passwordEncoder.encode(plainPassword);
     }
 
-    public boolean validatePasswordMatch(String plainPassword, String encodedPassword) {
-        return passwordEncoder.matches(plainPassword, encodedPassword);
+    public void validatePasswordMatch(String plainPassword, String encodedPassword) {
+        boolean matches = passwordEncoder.matches(plainPassword, encodedPassword);
+        if (!matches){
+            throw new RootException(ResultCodeType.SERVER_ERROR_4S000000);
+        }
     }
 }
