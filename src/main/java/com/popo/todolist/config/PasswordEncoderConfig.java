e4ae8fdc119bb31a41380b12ae4ef66bf701eb08
popo <kst005109@naver.com>
2024-06-20 14:13:03 +0900

    Rename : config 파일 경로 변경

--- src/main/java/com/popo/todolist/config/PasswordEncoderConfig.java
+++ src/main/java/com/popo/todolist/config/PasswordEncoderConfig.java
@@ -1,4 +1,4 @@
-package com.popo.todolist.component;
+package com.popo.todolist.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/popo/todolist/config/SecurityConfig.java
+++ src/main/java/com/popo/todolist/config/SecurityConfig.java
@@ -1,15 +1,14 @@
-package com.popo.todolist.component;
+package com.popo.todolist.config;
 
+import com.popo.todolist.component.JwtUtil;
 import com.popo.todolist.component.security.JwtAuthFilter;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
